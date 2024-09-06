4f166a134efe2cf221326f1c7585707c3482ba54
jutionck <jutionck@gmail.com>
2024-09-06 16:23:27 +0700

    Data Penduduk Starter Pack

---
+++ src/main/java/com/mandiri/datapenduduk/LivecodeSpringbootDataPendudukStarterPackApplication.java
@@ -0,0 +1,13 @@
+package com.mandiri.datapenduduk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivecodeSpringbootDataPendudukStarterPackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivecodeSpringbootDataPendudukStarterPackApplication.class, args);
+    }
+
+}
